9a527e082bac775aac5bde2bb3002d22cbcdf411
rsuter_cp <SND\rsuter_cp>
2011-11-12 13:38:05 -0800

    - added translated TranslatedToggleSwitch

---
+++ UI/TranslatedToggleSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MyToolkit.UI
+{
+	public class TranslatedToggleSwitch : ToggleSwitch
+	{
+		public TranslatedToggleSwitch()
+		{
+			UpdateText(null, null);
+			Checked += UpdateText;
+			Unchecked += UpdateText;
+		}
+
+		private void UpdateText(object sender, RoutedEventArgs routedEventArgs)
+		{
+			var isChecked = IsChecked.HasValue && IsChecked.Value;
+			var text = isChecked ? "On" : "Off";
+				
+			switch (CultureInfo.CurrentCulture.TwoLetterISOLanguageName)
+			{
+				case "de": text = isChecked ? "Ein" : "Aus"; break;
+				case "fr": text = isChecked ? "Activé" : "Désactivé"; break;
+			}
+
+			Content = text; 
+		}
+	}
+}
