45ecb6ea3a6f8c91ad2288cacd381aa460adae08
rsuter_cp <SND\rsuter_cp>
2012-08-09 15:14:23 -0700

    - pan and zoom image

---
+++ Shared/Converters/ControlClippingConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MyToolkit.Converters
+{
+	public class ControlClippingConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var ctrl = (FrameworkElement) value;
+			return new Rect(0, 0, ctrl.ActualWidth, ctrl.ActualHeight);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
