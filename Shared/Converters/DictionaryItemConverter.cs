ade4a99167ca4229b3b70b6ab02fd1030a834a97
rsuter_cp <SND\rsuter_cp>
2012-08-28 14:11:06 -0700

    - Added DictionaryItemConverter to bind to dictionaries - Added custom DataBinding binding to use in shape XAML

---
+++ Shared/Converters/DictionaryItemConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+#if !METRO
+using System.Windows.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace MyToolkit.Converters
+{
+	public class DictionaryItemConverter : IValueConverter
+	{
+#if !METRO
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return "";
+
+			if (parameter == null)
+				parameter = "datetime";
+
+			switch (parameter.ToString().ToLower())
+			{
+				case "date": return ((DateTime)value).ToShortDateString();
+				case "time": return ((DateTime)value).ToShortTimeString();
+				default: return ((DateTime)value).ToShortDateString() + " " + ((DateTime)value).ToShortTimeString();
+			}
+		}
+#else
+		public object Convert(object value, Type typeName, object parameter, string language)
+
+		{
+			var dict = (IDictionary)value;
+			var path = parameter.ToString().Split('.');
+			
+			for (var i = 0; i < path.Length; i++)
+			{
+				var key = path[i];
+				if (i < path.Length - 1)
+				{
+					if (dict.Contains(key) && dict[key] is IDictionary)
+						dict = (IDictionary)dict[key];
+					else
+						return null; 
+				}
+				else
+				{
+					if (dict.Contains(key))
+						return dict[key];
+					return null; 
+				}
+			}
+			return null; 
+		}
+#endif
+
+#if !METRO
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+#else
+        public object ConvertBack(object value, Type typeName, object parameter, string language)
+#endif
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
