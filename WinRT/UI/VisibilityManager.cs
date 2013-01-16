7b7bbfe9320fb08a04e2fbbb910595fc4ee1df09
rsuter_cp <SND\rsuter_cp>
2013-01-16 05:52:17 -0800

    added snapped view (only logo)

--- WinRT/UI/VisibilityManager.cs
+++ WinRT/UI/VisibilityManager.cs
@@ -6,10 +6,12 @@ namespace MyToolkit.UI
 {
 	public static class VisibilityManager
 	{
+		#region Dependency properties
+
 		public static readonly DependencyProperty MinPageWidthProperty =
 			DependencyProperty.RegisterAttached("MinPageWidth", typeof(double), 
 				typeof(VisibilityManager), new PropertyMetadata(default(double), 
-					OnMinPageWidthChanged));
+					OnValueChanged));
 
 		public static void SetMinPageWidth(UIElement element, double value)
 		{
@@ -20,9 +22,26 @@ namespace MyToolkit.UI
 		{
 			return (double) element.GetValue(MinPageWidthProperty);
 		}
+		
+		public static readonly DependencyProperty MaxPageWidthProperty =
+			DependencyProperty.RegisterAttached("MaxPageWidth", typeof(double),
+				typeof(VisibilityManager), new PropertyMetadata(default(double),
+					OnValueChanged));
+
+		public static void SetMaxPageWidth(UIElement element, double value)
+		{
+			element.SetValue(MaxPageWidthProperty, value);
+		}
+
+		public static double GetMaxPageWidth(UIElement element)
+		{
+			return (double) element.GetValue(MaxPageWidthProperty);
+		}
+
+		#endregion 
 
 		private static List<FrameworkElement> elements;  
-		private static void OnMinPageWidthChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+		private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
 		{
 			var element = obj as FrameworkElement;
 			if (element != null)
@@ -32,14 +51,13 @@ namespace MyToolkit.UI
 				element.Loaded += delegate(object s, RoutedEventArgs e)
 				{
 					var sender = (FrameworkElement)s;
-					var value = GetMinPageWidth(sender);
-					if (value > 0)
+					if (GetMinPageWidth(sender) > 0 || GetMaxPageWidth(sender) > 0)
 					{
 						Initialize();
 						if (!elements.Contains(sender))
 						{
 							elements.Add(sender);
-							UpdateElement(sender, value);
+							UpdateElement(sender, Window.Current.Bounds.Width);
 						}
 					}
 				};
@@ -53,17 +71,28 @@ namespace MyToolkit.UI
 						CleanUp();
 				};
 
-				UpdateElement(element, (double)args.NewValue);
+				UpdateElement(element, Window.Current.Bounds.Width);
 
 				if (!elements.Contains(element))
 					elements.Add(element);
 			}
 		}
 
-		private static void UpdateElement(FrameworkElement ctrl, double value)
+		private static void UpdateElement(FrameworkElement element, double windowWidth)
 		{
-			ctrl.Visibility = Window.Current.Bounds.Width < value ? 
-				Visibility.Collapsed : Visibility.Visible;
+			var minWidth = GetMinPageWidth(element);
+			var maxWidth = GetMaxPageWidth(element);
+			
+			if (minWidth > 0)
+			{
+				if (minWidth > 0 && maxWidth > 0)
+					element.Visibility = windowWidth > minWidth && Window.Current.Bounds.Width <= maxWidth ?
+						Visibility.Visible : Visibility.Collapsed;
+				else
+					element.Visibility = windowWidth > minWidth ? Visibility.Visible : Visibility.Collapsed;
+			} 
+			else if (maxWidth > 0)
+				element.Visibility = windowWidth <= maxWidth ? Visibility.Visible : Visibility.Collapsed;
 		}
 
 		private static void Initialize()
@@ -78,10 +107,7 @@ namespace MyToolkit.UI
 		private static void OnSizeChanged(object sender, WindowSizeChangedEventArgs args)
 		{
 			foreach (var e in elements)
-			{
-				e.Visibility = args.Size.Width < GetMinPageWidth(e) ? 
-					Visibility.Collapsed : Visibility.Visible;
-			}
+				UpdateElement(e, args.Size.Width);
 		}
 
 		private static void CleanUp()
