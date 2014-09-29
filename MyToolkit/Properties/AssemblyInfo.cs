d0255138dcc989c29f59c406951e4ac83742f3c4
Rico Suter <mail@rsuter.com>
2014-09-29 21:55:18 +0200

    updated version

--- MyToolkit/Properties/AssemblyInfo.cs
+++ MyToolkit/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("2.3.13.0")]
-[assembly: AssemblyFileVersion("2.3.13.0")]
+[assembly: AssemblyVersion("2.3.14.0")]
+[assembly: AssemblyFileVersion("2.3.14.0")]
