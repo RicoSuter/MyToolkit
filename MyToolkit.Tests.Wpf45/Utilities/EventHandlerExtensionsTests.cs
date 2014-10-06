bd8f798e48e0f4c2d2c980cb2b614c9cf279858e
Rico Suter <mail@rsuter.com>
2014-10-06 20:02:42 +0200

    added EventHandlerExtensions

---
+++ MyToolkit.Tests.Wpf45/Utilities/EventHandlerExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventHandlerExtensionsTests.cs" company="MyToolkit">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>http://mytoolkit.codeplex.com/license</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyToolkit.Utilities;
+
+namespace MyToolkit.Tests.Wpf45.Utilities
+{
+    [TestClass]
+    public class EventHandlerExtensionsTests
+    {
+        [TestMethod]
+        public void When_raising_event_then_callback_should_be_called()
+        {
+            //// Arrange
+            var count = 0; 
+            var obj = new EventTestStub();
+            obj.PropertyChanged += delegate { count++; };
+
+            //// Act
+            obj.RaiseEvent();
+
+            //// Assert
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void When_no_callback_is_registered_then_raise_should_not_throw_error()
+        {
+            //// Arrange
+            var count = 0;
+            var obj = new EventTestStub();
+
+            //// Act
+            obj.RaiseEvent();
+
+            //// Assert
+            Assert.AreEqual(0, count);
+        }
+    }
+
+    public class EventTestStub : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaiseEvent()
+        {
+            PropertyChanged.Raise(null, new PropertyChangedEventArgs("test"));
+        }
+    }
+}
---
+++ MyToolkit/Utilities/EventHandlerExtensions.cs
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventHandlerExtensions.cs" company="MyToolkit">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>http://mytoolkit.codeplex.com/license</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace MyToolkit.Utilities
+{
+    /// <summary>Provides extension methods for event handlers. </summary>
+    public static class EventHandlerExtensions
+    {
+        /// <summary>Raises the event in a thead-safe manner. </summary>
+        /// <param name="handler">The event handler. </param>
+        /// <param name="sender">The sender</param>
+        /// <param name="args">The event arguments</param>
+#if !LEGACY
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static void Raise(this EventHandler handler, object sender, EventArgs args)
+        {
+            var copy = handler;
+            if (copy != null)
+                copy(sender, args);
+        }
+
+        /// <summary>Raises the event in a thead-safe manner. </summary>
+        /// <typeparam name="TEventArgs">The type of the event arguments. </typeparam>
+        /// <param name="handler">The event handler. </param>
+        /// <param name="sender">The sender</param>
+        /// <param name="args">The event arguments</param>
+#if !LEGACY
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static void Raise<TEventArgs>(this EventHandler<TEventArgs> handler, object sender, TEventArgs args)
+            where TEventArgs : EventArgs
+        {
+            var copy = handler;
+            if (copy != null)
+                copy(sender, args);
+        }
+
+        /// <summary>Raises the event in a thead-safe manner. </summary>
+        /// <typeparam name="TEventHandler">The type of the event handler. </typeparam>
+        /// <typeparam name="TEventArgs">The type of the event arguments. </typeparam>
+        /// <param name="handler">The event handler. </param>
+        /// <param name="sender">The sender</param>
+        /// <param name="args">The event arguments</param>
+#if !LEGACY
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static void Raise<TEventHandler, TEventArgs>(this TEventHandler handler, object sender, TEventArgs args)
+            where TEventArgs : EventArgs
+            where TEventHandler : class
+        {
+            var copy = handler;
+            if (copy != null)
+            {
+#if !LEGACY
+                var info = copy.GetType().GetRuntimeMethod("Invoke", new[] { typeof(object), typeof(TEventArgs) });
+#else
+                var info = copy.GetType().GetMethod("Invoke", new[] { typeof(object), typeof(TEventArgs) });
+#endif
+                info.Invoke(copy, new[] { sender, args });
+            }
+        }
+    }
+}
