7c034c46a0fd329dc0f7ff29ebc62a0b70caed96
Rico Suter <mail@rsuter.com>
2014-12-02 22:47:30 +0100

    Added person class

---
+++ SampleWindowsStoreApp/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleWindowsStoreApp.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Category { get; set; }
+    }
+}
