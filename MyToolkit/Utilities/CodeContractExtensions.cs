839c58f8851f2443610ac02df89571c0bd176943
Rico Suter <mail@rsuter.com>
2014-12-13 00:54:53 +0100

    Improved CodeContractExtensions

--- MyToolkit/Utilities/CodeContractExtensions.cs
+++ MyToolkit/Utilities/CodeContractExtensions.cs
@@ -29,8 +29,8 @@ namespace MyToolkit.Utilities
         public static void CheckNotNullOrEmpty([ValidatedNotNullAttribute] this string value, string parameterName)
         {
 #if !LEGACY
-            Contract.Requires(value != null, "The value '" + parameterName + "' cannot be null. ");
-            Contract.Requires(value != string.Empty, "The string '" + parameterName + "' cannot be empty. ");
+            Contract.Requires<ArgumentNullException>(value != null, "The value '" + parameterName + "' cannot be null. ");
+            Contract.Requires<ArgumentNullException>(value != string.Empty, "The string '" + parameterName + "' cannot be empty. ");
 #endif
 
             if (string.IsNullOrEmpty(value))
@@ -48,11 +48,33 @@ namespace MyToolkit.Utilities
         [ContractAbbreviator]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static void CheckNotNull<T>([ValidatedNotNullAttribute] this T value, string parameterName)
-          where T : class
+        public static void CheckNotNull([ValidatedNotNullAttribute] this object value, string parameterName)
         {
 #if !LEGACY
-            Contract.Requires(value != null, "The value '" + parameterName + "' cannot be null. ");
+            Contract.Requires<ArgumentNullException>(value != null, "The value '" + parameterName + "' cannot be null. ");
+#endif
+
+            if (value == null)
+                throw new ArgumentNullException(parameterName);
+        }
+
+        /// <summary>Throws an <see cref="ArgumentNullException" /> if the value is <c>null</c>.</summary>
+        /// <typeparam name="T">The type of the value. </typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        /// <exception cref="ArgumentNullException">The value cannot be null.</exception>
+        /// <remarks>The method throws a <see cref="ArgumentNullException" /> and also defines a Contract.Requires statement for static analysis.</remarks>
+        [DebuggerStepThrough]
+#if !LEGACY
+        [ContractAbbreviator]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static void CheckNotNull<T>([ValidatedNotNullAttribute] this T? value, string parameterName)
+            where T : struct
+        {
+            // This method is additionally needed so that nullable objects are not boxed to object
+#if !LEGACY
+            Contract.Requires<ArgumentNullException>(value != null, "The value '" + parameterName + "' cannot be null. ");
 #endif
 
             if (value == null)
@@ -66,5 +88,4 @@ namespace MyToolkit.Utilities
         {
         }
     }
-
 }
