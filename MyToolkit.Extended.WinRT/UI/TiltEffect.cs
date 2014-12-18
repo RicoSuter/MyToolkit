1f531d476b54d408d8d6d462a00d4266015623ff
Rico Suter <mail@rsuter.com>
2014-12-18 08:29:48 +0100

    Added XML doc for TiltEffect

--- MyToolkit.Extended.WinRT/UI/TiltEffect.cs
+++ MyToolkit.Extended.WinRT/UI/TiltEffect.cs
@@ -1,20 +1,34 @@
-﻿using Windows.UI.Xaml;
+﻿//-----------------------------------------------------------------------
+// <copyright file="TiltEffect.cs" company="MyToolkit">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>http://mytoolkit.codeplex.com/license</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Animation;
 
 namespace MyToolkit.UI
 {
+    /// <summary>Provides an attached property to enable the tilt effect (push down/up animation) for <see cref="UIElement"/>s. </summary>
 	public static class TiltEffect
 	{
 		public static readonly DependencyProperty IsTiltEnabledProperty =
 			DependencyProperty.RegisterAttached("IsTiltEnabled", typeof (bool), typeof (TiltEffect), new PropertyMetadata(default(bool), OnIsTiltEnabledChanged ));
 
+        /// <summary>Sets a value indicating whether to enable tilt effect for the <see cref="UIElement"/>. </summary>
+        /// <param name="element">The element. </param>
+        /// <param name="value">The value. </param>
 		public static void SetIsTiltEnabled(UIElement element, bool value)
 		{
 			element.SetValue(IsTiltEnabledProperty, value);
 		}
 
-		public static bool GetIsTiltEnabled(UIElement element)
+        /// <summary>Gets a value indicating whether the tilt effect for the <see cref="UIElement"/> is enabled. </summary>
+        /// <param name="element">The element. </param>
+        public static bool GetIsTiltEnabled(UIElement element)
 		{
 			return (bool) element.GetValue(IsTiltEnabledProperty);
 		}
@@ -22,7 +36,7 @@ namespace MyToolkit.UI
 		private static void OnIsTiltEnabledChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
 		{
 			var element = (UIElement)obj;
-			if ((bool)args.NewValue)
+            if ((bool)args.NewValue)
 			{
 				element.PointerPressed += OnPointerPressed;
 				element.PointerReleased += OnPointerReleased;
@@ -62,7 +76,7 @@ namespace MyToolkit.UI
 		private static void ShowUpAnimation(UIElement element)
 		{
 			var animation = new PointerUpThemeAnimation();
-			Storyboard.SetTarget(animation, element);
+            Storyboard.SetTarget(animation, element);
 
 			var storyboard = new Storyboard();
 			storyboard.Children.Add(animation);
