82152c1170c6198d33fb73647bf435d1d6e5a6fb
Rico Suter <mail@rsuter.com>
2015-01-28 18:40:11 +0100

    Release v2.3.25

--- MyToolkit/Properties/AssemblyInfo.cs
+++ MyToolkit/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("2.3.24.0")]
-[assembly: AssemblyFileVersion("2.3.24.0")]
+[assembly: AssemblyVersion("2.3.25.0")]
+[assembly: AssemblyFileVersion("2.3.25.0")]
