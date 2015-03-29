3303fb8495d32a8e55dbaa60d738c5ac5a2ed4d0
Rico Suter <mail@rsuter.com>
2015-03-29 01:27:54 +0100

    Added asp.net mvc dependency resolver

---
+++ MyToolkit.AspNet.Mvc/Composition/AspNetMvcControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MyToolkit.Composition
+{
+    public class AspNetMvcControllerFactory : DefaultControllerFactory
+    {
+        private readonly CompositionContext _compositionContext;
+
+        public AspNetMvcControllerFactory(CompositionContext compositionContext)
+        {
+            _compositionContext = compositionContext;
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+                return null; 
+
+            var constructor = controllerType.GetConstructors().First();
+
+            var parameters = constructor
+                .GetParameters()
+                .Select(argument => _compositionContext.GetPart(argument.ParameterType, null))
+                .ToArray();
+
+            return (IController)constructor.Invoke(parameters);
+        }
+    }
+}
---
+++ MyToolkit.AspNet.Mvc/Composition/AspNetMvcDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MyToolkit.Composition
+{
+    public class AspNetMvcDependencyResolver : IDependencyResolver
+    {
+        private readonly CompositionContext _compositionContext;
+
+        public AspNetMvcDependencyResolver(CompositionContext compositionContext)
+        {
+            _compositionContext = compositionContext;
+
+            var factory = new AspNetMvcControllerFactory(_compositionContext);
+            _compositionContext.AddPart<IControllerFactory, AspNetMvcControllerFactory>(factory);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _compositionContext.GetPart(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _compositionContext.GetParts(serviceType);
+        }
+    }
+}
