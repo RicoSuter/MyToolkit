8a3ae52f6e3609c64420c0e6da75ad5b9d099521
Rico Suter <mail@rsuter.com>
2015-04-08 21:46:05 +0200

    Added image upload extensions

--- MyToolkit.AspNet.Mvc/Html/CollectionEditorExtensions.cs
+++ MyToolkit.AspNet.Mvc/Html/CollectionEditorExtensions.cs
@@ -22,43 +22,43 @@ namespace MyToolkit.Html
         /// <summary>Renders the collection editor for an enumerable.</summary>
         /// <typeparam name="TModel">The type of the model.</typeparam>
         /// <typeparam name="TItem">The type of the item.</typeparam>
-        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="html">The HTML helper.</param>
         /// <param name="collection">The collection.</param>
         /// <param name="partialViewName">The partial name of the editor view.</param>
         /// <param name="controllerActionPath">The controller action path to generate a new item editor.</param>
         /// <param name="addButtonTitle">The title of the add button.</param>
         /// <param name="addButtonHtmlAttributes">The HTML attributes of the add button.</param>
         /// <returns>The HTML string. </returns>
-        public static IHtmlString CollectionEditorFor<TModel, TItem>(this HtmlHelper<TModel> htmlHelper,
+        public static IHtmlString CollectionEditorFor<TModel, TItem>(this HtmlHelper<TModel> html,
             Func<TModel, IEnumerable<TItem>> collection, string partialViewName,
             string controllerActionPath, string addButtonTitle, object addButtonHtmlAttributes = null)
         {
             var editorId = "CollectionEditor_" + Guid.NewGuid().ToString("N");
             var addButtonId = "CollectionEditorAdd_" + Guid.NewGuid().ToString("N");
 
-            var html = new StringBuilder();
+            var output = new StringBuilder();
 
-            RenderInitialCollection(html, htmlHelper, collection, partialViewName, editorId);
-            RenderAddButton(html, addButtonId, addButtonTitle, addButtonHtmlAttributes);
-            RenderEditorScript(controllerActionPath, html, editorId, addButtonId);
+            RenderInitialCollection(output, html, collection, partialViewName, editorId);
+            RenderAddButton(output, addButtonId, addButtonTitle, addButtonHtmlAttributes);
+            RenderEditorScript(controllerActionPath, output, editorId, addButtonId);
 
-            return new HtmlString(html.ToString());
+            return new HtmlString(output.ToString());
         }
 
-        private static void RenderInitialCollection<TModel, TItem>(StringBuilder html, HtmlHelper<TModel> htmlHelper,
+        private static void RenderInitialCollection<TModel, TItem>(StringBuilder output, HtmlHelper<TModel> html,
             Func<TModel, IEnumerable<TItem>> collection, string partialViewName, string editorId)
         {
-            html.AppendLine(@"<ul id=""" + editorId + @""" style=""list-style-type: none; padding: 0"">");
-            var items = collection(htmlHelper.ViewData.Model);
+            output.AppendLine(@"<ul id=""" + editorId + @""" style=""list-style-type: none; padding: 0"">");
+            var items = collection(html.ViewData.Model);
             if (items != null)
             {
-                foreach (var item in collection(htmlHelper.ViewData.Model))
-                    html.AppendLine(htmlHelper.Partial(partialViewName, item).ToString());
+                foreach (var item in collection(html.ViewData.Model))
+                    output.AppendLine(html.Partial(partialViewName, item).ToString());
             }
-            html.AppendLine(@"</ul>");
+            output.AppendLine(@"</ul>");
         }
 
-        private static void RenderAddButton(StringBuilder html, string addButtonId, string addButtonTitle, object addButtonHtmlAttributes)
+        private static void RenderAddButton(StringBuilder output, string addButtonId, string addButtonTitle, object addButtonHtmlAttributes)
         {
             var inputTag = new TagBuilder("input");
             inputTag.Attributes.Add("type", "button");
@@ -66,14 +66,14 @@ namespace MyToolkit.Html
             inputTag.Attributes.Add("id", addButtonId);
             inputTag.MergeAttributes(new RouteValueDictionary(addButtonHtmlAttributes));
 
-            html.AppendLine(@"<p>");
-            html.AppendLine(inputTag.ToString());
-            html.AppendLine(@"</p>");
+            output.AppendLine(@"<p>");
+            output.AppendLine(inputTag.ToString());
+            output.AppendLine(@"</p>");
         }
 
-        private static void RenderEditorScript(string controllerActionPath, StringBuilder html, string editorId, string addButtonId)
+        private static void RenderEditorScript(string controllerActionPath, StringBuilder output, string editorId, string addButtonId)
         {
-            html.AppendLine(
+            output.AppendLine(
                 @"<script type=""text/javascript"">
                     $(function() {
                         $(""#" + editorId + @""").sortable();
@@ -95,10 +95,10 @@ namespace MyToolkit.Html
 
         /// <summary>Begins the rendering of collection item editor.</summary>
         /// <typeparam name="TModel">The type of the model.</typeparam>
-        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="html">The HTML helper.</param>
         /// <param name="collectionPropertyName">The name of the collection property in the master view model.</param>
         /// <returns>The disposable. </returns>
-        public static IDisposable BeginCollectionItem<TModel>(this HtmlHelper<TModel> htmlHelper, string collectionPropertyName)
+        public static IDisposable BeginCollectionItem<TModel>(this HtmlHelper<TModel> html, string collectionPropertyName)
         {
             var itemIndex = GetCollectionItemIndex(collectionPropertyName);
             var collectionItemName = String.Format("{0}[{1}]", collectionPropertyName, itemIndex);
@@ -112,8 +112,8 @@ namespace MyToolkit.Html
                 { "autocomplete", "off" }
             });
 
-            htmlHelper.ViewContext.Writer.WriteLine(hiddenInput.ToString(TagRenderMode.SelfClosing));
-            return new CollectionItemNamePrefixScope(htmlHelper.ViewData.TemplateInfo, collectionItemName);
+            html.ViewContext.Writer.WriteLine(hiddenInput.ToString(TagRenderMode.SelfClosing));
+            return new CollectionItemNamePrefixScope(html.ViewData.TemplateInfo, collectionItemName);
         }
 
         private static string GetCollectionItemIndex(string collectionIndexFieldName)
---
+++ MyToolkit.AspNet.Mvc/Html/ImageUploadExtensions.cs
@@ -0,0 +1,155 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CollectionEditorExtensions.cs" company="MyToolkit">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>http://mytoolkit.codeplex.com/license</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyToolkit.Html
+{
+    public static class ImageUploadExtensions
+    {
+        /// <summary>Renders the image upload control to upload a required image.</summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="html">The HTML helper.</param>
+        /// <param name="imageProperty">The image property.</param>
+        /// <returns>The HTML string. </returns>
+        public static IHtmlString RequiredImageUploadFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> imageProperty)
+        {
+            return RequiredImageUploadFor(html, imageProperty, null);
+        }
+
+        /// <summary>Renders the image upload control to upload a required image.</summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="html">The HTML helper.</param>
+        /// <param name="imageProperty">The image property.</param>
+        /// <param name="thumbnailPath">The image thumbnail path.</param>
+        /// <returns>The HTML string. </returns>
+        public static IHtmlString RequiredImageUploadFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> imageProperty,
+            string thumbnailPath)
+        {
+            var imagePathId = "ImagePath_" + Guid.NewGuid().ToString("N");
+
+            var imagePropertyMeta = ModelMetadata.FromLambdaExpression(imageProperty, html.ViewData);
+            var imagePropertyName = imagePropertyMeta.PropertyName;
+            
+            var output = new StringBuilder();
+            output.AppendLine(@"<div>");
+
+            if (!string.IsNullOrEmpty(thumbnailPath))
+            {
+                RenderImage(output, thumbnailPath);
+                RenderButton(output, imagePropertyName, imagePathId, "Change image...");
+            }
+            else
+                RenderButton(output, imagePropertyName, imagePathId, "Choose image...");
+
+            output.AppendLine(@"</div>");
+            return new HtmlString(output.ToString());
+        }
+
+        /// <summary>Renders the image upload control to upload an optional image.</summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="html">The HTML helper.</param>
+        /// <param name="imageProperty">The image property.</param>
+        /// <param name="hasImageProperty">The has image property.</param>
+        /// <returns>The HTML string. </returns>
+        public static IHtmlString ImageUploadFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> imageProperty,
+            Expression<Func<TModel, bool>> hasImageProperty)
+        {
+            return ImageUploadFor(html, imageProperty, hasImageProperty, null);
+        }
+
+        /// <summary>Renders the image upload control to upload an optional image.</summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="html">The HTML helper.</param>
+        /// <param name="imageProperty">The image property.</param>
+        /// <param name="hasImageProperty">The has image property.</param>
+        /// <param name="thumbnailPath">The image thumbnail path.</param>
+        /// <returns>The HTML string. </returns>
+        public static IHtmlString ImageUploadFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> imageProperty,
+            Expression<Func<TModel, bool>> hasImageProperty,
+            string thumbnailPath)
+        {
+            var imagePathId = "ImagePath_" + Guid.NewGuid().ToString("N");
+
+            var imagePropertyMeta = ModelMetadata.FromLambdaExpression(imageProperty, html.ViewData);
+            var hasImagePropertyMeta = ModelMetadata.FromLambdaExpression(hasImageProperty, html.ViewData);
+
+            var imagePropertyName = imagePropertyMeta.PropertyName;
+            var hasImagePropertyName = hasImagePropertyMeta.PropertyName;
+
+            var output = new StringBuilder();
+            output.AppendLine(@"<div>");
+
+            var hasImage = hasImageProperty != null && hasImageProperty.Compile().Invoke(html.ViewData.Model);
+            if (hasImage)
+            {
+                var noImageRadioId = "NoImageRadioId_" + Guid.NewGuid().ToString("N");
+                var currentImageRadioId = "CurrentImageRadioId_" + Guid.NewGuid().ToString("N");
+
+                if (!imagePropertyMeta.IsRequired)
+                {
+
+                    output.AppendLine(@"<div>");
+                    output.AppendLine(@"  <input type=""radio"" id=""" + noImageRadioId + @""" name=""" + hasImagePropertyName + @""" value=""false"">");
+                    output.AppendLine(@"  <label for=""" + noImageRadioId + @""">No image</label>");
+                    output.AppendLine(@"</div>");
+
+                    output.AppendLine(@"<div>");
+                    output.AppendLine(@"  <input type=""radio"" id=""" + currentImageRadioId + @""" name=""" + hasImagePropertyName + @""" value=""true"" checked=""checked"">");
+                    output.AppendLine(@"  <label for=""" + currentImageRadioId + @""">Current image:</label><br />");
+                    output.AppendLine(@"</div>");
+                }
+
+                RenderImage(output, thumbnailPath);
+                RenderButton(output, imagePropertyName, imagePathId, "Change image...");
+            }
+            else
+            {
+                if (!imagePropertyMeta.IsRequired)
+                    output.AppendLine(@"<input type=""hidden"" name=""" + hasImagePropertyName + @""" value=""true"" />");
+
+                RenderButton(output, imagePropertyName, imagePathId, "Choose image...");
+            }
+
+            output.AppendLine(@"</div>");
+            return new HtmlString(output.ToString());
+        }
+
+        private static void RenderImage(StringBuilder output, string thumbnailPath)
+        {
+            output.AppendLine(@"<p>");
+            output.AppendLine(@"  <img src=""" + thumbnailPath + @""" alt=""Thumbnail"" />");
+            output.AppendLine(@"</p>");
+        }
+
+        private static void RenderButton(StringBuilder output, string imagePropertyName, string imagePathId, string buttonText)
+        {
+            output.AppendLine(@"<div style=""position: relative"">");
+            output.AppendLine(@"  <a class=""btn btn-default"" href=""javascript:;"">" + buttonText);
+            output.AppendLine(@"    <input type=""file"" name=""" + imagePropertyName + @""" size=""40"" ");
+            output.AppendLine(@"           onchange='$(""#" + imagePathId + @""").html($(this).val());' ");
+            output.AppendLine(@"           style=""position: absolute; z-index: 2; top: 0; left: 0; filter: alpha(opacity=0);" +
+                              @"                   opacity: 0; background-color: transparent; color: transparent;""/>");
+            output.AppendLine(@"  </a>");
+            output.AppendLine(@"  <span class=""label label-info"" id=""" + imagePathId + @"""></span>");
+            output.AppendLine(@"</div>");
+        }
+    }
+}
