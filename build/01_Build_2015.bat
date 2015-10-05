5a54f446186acf2640c26b41affa0e6016cbe46e
Rico Suter <mail@rsuter.com>
2015-10-05 08:45:18 +0200

    Added nuget restore to build scripts

--- build/01_Build_2015.bat
+++ build/01_Build_2015.bat
@@ -1,1 +1,2 @@
+nuget restore ../src/MyToolkit.VS2015.sln
 msbuild ../src/MyToolkit.VS2015.sln /p:Configuration=Release /t:rebuild
--- build/02_Build_2012.bat
+++ build/02_Build_2012.bat
@@ -1,1 +1,2 @@
+nuget restore ../src/MyToolkit.VS2012.sln
 msbuild ../src/MyToolkit.VS2012.sln /p:Configuration=Release /t:rebuild
