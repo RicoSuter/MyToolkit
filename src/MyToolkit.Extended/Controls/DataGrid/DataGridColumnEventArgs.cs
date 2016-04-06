3f20d051d96db1690f37ef32bbdb3ab2f3a432f9
Bram Stoeller <bram@stoeller.nl>
2016-04-06 13:54:21 +0200

    Add an OrderChanged event to the DataGrid

---
+++ src/MyToolkit.Extended/Controls/DataGrid/DataGridColumnEventArgs.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataGridColumnEventArgs.cs" company="MyToolkit">
+//     Copyright (c) Bram Stoeller. All rights reserved.
+// </copyright>
+// <license>https://github.com/MyToolkit/MyToolkit/blob/master/LICENSE.md</license>
+// <author>Bram Stoeller, bram@stoeller.nl</author>
+//-----------------------------------------------------------------------
+
+#if WINRT
+
+using MyToolkit.Controls;
+using System;
+
+namespace MyToolkit.Controls
+{
+    public class DataGridColumnEventArgs : EventArgs
+    {
+        internal DataGridColumnEventArgs(DataGridColumnBase column)
+        {
+            Column = column;
+        }
+
+        /// <summary>Gets ordering column. </summary>
+        public DataGridColumnBase Column { private set; get; }
+
+    }
+}
+
+#endif
