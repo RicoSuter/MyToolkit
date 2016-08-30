afb1f8aa94ab8cbd4d06b6a34e62d066eeaa6910
Rico Suter <mail@rsuter.com>
2016-08-31 01:00:08 +0200

    v2.5.13

--- build/04_PushPackages.bat
+++ build/04_PushPackages.bat
@@ -1,8 +1,8 @@
 set /p apiKey=NuGet API Key: 
 set /p version=Package Version: 
 
-nuget.exe push Packages/MyToolkit.%version%.nupkg %apiKey%
-nuget.exe push Packages/MyToolkit.Web.%version%.nupkg %apiKey%
-nuget.exe push Packages/MyToolkit.Http.%version%.nupkg %apiKey%
-nuget.exe push Packages/MyToolkit.Extended.%version%.nupkg %apiKey%
-nuget.exe push Packages/MyToolkit.AspNet.Mvc.%version%.nupkg %apiKey%
+nuget.exe push Packages/MyToolkit.%version%.nupkg %apiKey% -s https://nuget.org
+nuget.exe push Packages/MyToolkit.Web.%version%.nupkg %apiKey% -s https://nuget.org
+nuget.exe push Packages/MyToolkit.Http.%version%.nupkg %apiKey% -s https://nuget.org
+nuget.exe push Packages/MyToolkit.Extended.%version%.nupkg %apiKey% -s https://nuget.org
+nuget.exe push Packages/MyToolkit.AspNet.Mvc.%version%.nupkg %apiKey% -s https://nuget.org
